a831c2960f4f0e471927a7ca48a8058aa3505d28
danish703 <dipu.danish@outlook.com>
2022-06-13 18:07:21 +0545

    initial

---
+++ LMSSystem/Controllers/DashBoardController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace LMSSystem.Controllers
+{
+    public class DashBoardController : Controller
+    {
+        // GET: /<controller>/
+        public ViewResult Dash()
+        {
+            return View();
+        }
+
+        public ViewResult Add()
+        {
+            return View();
+        }
+    }
+}
